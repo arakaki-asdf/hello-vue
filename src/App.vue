bb0d17e101b3be79e4884e556d4ee9ee0cf28ff6
arakaki hayate <hayateasdf@gmail.com>
2024-08-24 14:06:32 +0900

    watch 対応

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
-import { ref, computed} from "vue";
+import { ref, watch } from "vue";
 
-const list: string[] = ["hoge", "hogehoge", "world"];
-const cockList = ref(list);
-const changeList = (): void => {
-  cockList.value = ["バラライカ", "XYZ", "マンハッタン"];
+interface Data {
+  id: number;
+  name: string;
+  price: number;
 };
 
-const addCockList = (): void => {
-  cockList.value.push("ブルームーン");
-};
-const deleteFromCockList = (): void => {
-  cockList.value.pop();
-};
+// テンプレート変数
+const dataNo = ref(1);
+const priceMessage = ref("");
+
+watch(dataNo, (): void => {
+    priceMessage.value = getCocktailInfo(dataNo.value);
+  }
+);
+
+function getCocktailInfo(no: number): string {
+  // データ
+  const map = new Map<number, Data>();
+  map.set(1, {id: 1, name: "white", price: 1200});
+  map.set(2, {id: 2, name: "blue", price: 1500});
+  map.set(3, {id: 3, name: "new", price: 1100});
+  map.set(4, {id: 4, name: "mat", price: 1500});
+  const data = map.get(no);
+
+  return data === undefined
+    ? "該当カクテルはありません。"
+    : `該当するカクテルは${data.name}で、価格は${data.price}円です`;
+}
+
+setInterval(
+  (): void => {
+    dataNo.value = Math.round(Math.random() * 3) + 1;
+  },
+  1000);
 </script>
 
 <template>
-  <ul>
-    <li
-      v-for="(name, index) in cockList"
-      v-bind:key="name">
-      {{ name }}({{ index }})
-    </li>
-  </ul>
-
-  <p><button v-on:click="changeList">Change</button></p>
-  <p><button v-on:click="addCockList">Add</button></p>
-  <p><button v-on:click="deleteFromCockList">Delete</button></p>
+  <p>現在のカクテル番号: {{ dataNo }}</p>
+  <p>{{ priceMessage }}</p>
 </template>
