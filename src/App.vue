8a0a7532be8da4f5ded21650146af95b18529357
arakaki hayate <hayateasdf@gmail.com>
2024-08-24 15:35:28 +0900

    ライフサイクル

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,41 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, computed, onBeforeMount, onMounted, 
+    onBeforeUpdate, onUpdated, onRenderTracked, onRenderTriggered } from "vue";
+import type { DebuggerEvent } from "vue";
 
-interface Data {
-  id: number;
-  name: string;
-  price: number;
+const heightInit = Math.round(Math.random() * 10);
+const widthInit = Math.round(Math.random() * 10);
+const height = ref(heightInit);
+const width = ref(widthInit);
+const area = computed((): number => {
+    return height.value * width.value;
+  });
+const change = (): void => {
+  height.value = Math.round(Math.random() * 10);
+  width.value = Math.round(Math.random() * 10);
 };
 
-// テンプレート変数
-const dataNo = ref(1);
-const priceMessage = ref("");
-
-watch(dataNo, (): void => {
-    priceMessage.value = getCocktailInfo(dataNo.value);
-  }
-);
-
-function getCocktailInfo(no: number): string {
-  // データ
-  const map = new Map<number, Data>();
-  map.set(1, {id: 1, name: "white", price: 1200});
-  map.set(2, {id: 2, name: "blue", price: 1500});
-  map.set(3, {id: 3, name: "new", price: 1100});
-  map.set(4, {id: 4, name: "mat", price: 1500});
-  const data = map.get(no);
-
-  return data === undefined
-    ? "該当カクテルはありません。"
-    : `該当するカクテルは${data.name}で、価格は${data.price}円です`;
-}
-
-setInterval(
-  (): void => {
-    dataNo.value = Math.round(Math.random() * 3) + 1;
-  },
-  1000);
+onBeforeMount((): void => {
+  console.log(`beforeMount: ${height.value} * ${width.value}`);
+});
+onMounted((): void => {
+  console.log(`mounted: ${height.value} * ${width.value}`);
+});
+onBeforeUpdate((): void => {
+  console.log(`beforeUpdate: ${height.value} * ${width.value}`);
+});
+onUpdated((): void => {
+  console.log(`updated: ${height.value} * ${width.value}`);
+});
+onRenderTracked((): void => {
+  console.log(`renderTracked: ${height.value} * ${width.value}`);
+});
+onRenderTriggered((): void => {
+  console.log(`renderTriggered: ${height.value} * ${width.value}`);
+});
 </script>
 
 <template>
-  <p>現在のカクテル番号: {{ dataNo }}</p>
-  <p>{{ priceMessage }}</p>
+  <p>縦が{{ height }}、横が{{ width }}の長方形の面積は{{ area }}</p>
+  <button v-on:click="change">値を変更</button>
 </template>
