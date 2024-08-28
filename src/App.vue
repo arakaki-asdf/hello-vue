e87f9c17f20e55bb1a0ed1b6d4cecb47ee5059d3
arakaki hayate <hayateasdf@gmail.com>
2024-08-29 08:58:29 +0900

    コンポーネント削除、OneSection.vueを作って、App.vueで利用

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,18 @@
 <script setup lang="ts">
-import { ref, computed, onBeforeMount, onMounted, 
-    onBeforeUpdate, onUpdated, onRenderTracked, onRenderTriggered } from "vue";
-import type { DebuggerEvent } from "vue";
-
-const heightInit = Math.round(Math.random() * 10);
-const widthInit = Math.round(Math.random() * 10);
-const height = ref(heightInit);
-const width = ref(widthInit);
-const area = computed((): number => {
-    return height.value * width.value;
-  });
-const change = (): void => {
-  height.value = Math.round(Math.random() * 10);
-  width.value = Math.round(Math.random() * 10);
-};
-
-onBeforeMount((): void => {
-  console.log(`beforeMount: ${height.value} * ${width.value}`);
-});
-onMounted((): void => {
-  console.log(`mounted: ${height.value} * ${width.value}`);
-});
-onBeforeUpdate((): void => {
-  console.log(`beforeUpdate: ${height.value} * ${width.value}`);
-});
-onUpdated((): void => {
-  console.log(`updated: ${height.value} * ${width.value}`);
-});
-onRenderTracked((): void => {
-  console.log(`renderTracked: ${height.value} * ${width.value}`);
-});
-onRenderTriggered((): void => {
-  console.log(`renderTriggered: ${height.value} * ${width.value}`);
-});
+import OneSection from "./components/OneSection.vue"
 </script>
 
 <template>
-  <p>縦が{{ height }}、横が{{ width }}の長方形の面積は{{ area }}</p>
-  <button v-on:click="change">値を変更</button>
+  <h1>App Component</h1>
+  <section>
+    <h2>section 1</h2>
+    <OneSection />
+  </section>
+  <section>
+    <h2>コンポーネントが複数</h2>
+
+    <OneSection />
+    <OneSection />
+    <OneSection />
+  </section>
 </template>
---
+++ src/components/OneSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="box">
+        <h4>one component</h4>
+        <p>コンポーネントです。</p>
+    </section>
+</template>
+
+<style>
+.box {
+    border: gren 1px dashed;
+    margin: 10px;
+}
+</style>
+
