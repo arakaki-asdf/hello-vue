8c7566e4d1f751de82a4b3ade1a2c7dc598a95ce
arakaki hayate <hayateasdf@gmail.com>
2024-08-29 10:34:43 +0900

    Emitsを利用して子から親イベントの実行、リアクティブシステムにより親の変更を子に連動

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,21 @@
 import { ref, computed } from "vue";
 import OneSection from "./components/OneSection.vue";
 
-interface Member {
-  id: number;
-  name: string;
-  email: string;
-  points: number;
-  note?: string;
+let rand = Math.round(Math.random() * 10);
+const randRef = ref(rand);
+const onCreateNewRand = (): void => {
+  // randRef.value = Math.round(Math.random() * 10);
+  randRef.value = Math.round(Math.random() * 10);
 }
-
-const list = new Map<number, Member>();
-list.set(1, { id: 1, name: "田中太郎", email:"bow@example.com", points: 35, note: "初回入会特典あり" });
-list.set(2, { id: 2, name: "鈴木太郎", email:"mue@example.com", points: 53 });
-const memberList = ref(list);
-
-const totalPoints = computed((): number => {
-  let total = 0;
-  for (const member of memberList.value.values()) {
-    total += member.points;
-  }
-  return total;
-});
 </script>
 
 <template>
-  <h1>Props基礎</h1>
   <section>
-    <h2>属性に直接記述</h2>
-    <p>全会員の保有ポイントの合計: {{ totalPoints }}</p>
-    <OneSection 
-      v-for="[id, item] in list"
-      v-bind:key="id"
-      v-bind:id="id"
-      v-bind:name="item.name"
-      v-bind:email="item.email"
-      v-bind:points="item.points"
-      v-bind:note="item.note"
+    <h1>親コンポーネント</h1>
+    <!-- データ、実行するイベントを渡す -->
+    <OneSection
+      v-bind:rand="randRef"
+      v-on:createNewRand="onCreateNewRand"
     />
   </section>
 </template>
--- src/components/OneSection.vue
+++ src/components/OneSection.vue
@@ -1,41 +1,28 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-
+// parent -> this
 interface Props {
-    id: number;
-    name: string;
-    email: string;
-    points: number;
-    note?: string;
+    rand: number;
 }
 
-const props = withDefaults(
-    defineProps<Props>(),
-    {note: "--"}
-);
-const localPoints = ref(props.points);
-const pointUp = (): void => {
-    localPoints.value++;
-};
+// this -> parent (event fire)
+interface Emits {
+    (event: "createNewRand"): void;
+}
+
+// Propsを適用
+defineProps<Props>();
+
+// Emitの定義、実行用イベント
+const emit = defineEmits<Emits>();
+const onNewRandButtonClick = (): void => {
+    emit("createNewRand");
+}
 </script>
 
 <template>
     <section class="box">
-        <h4>{{ name }}さんの情報</h4>
-        <dl>
-            <dt>ID</dt>
-            <dd>{{ id }}</dd>
-
-            <dt>mail</dt>
-            <dd>{{ email }}</dd>
-
-            <dt>保有ポイント</dt>
-            <dd>{{ localPoints }}</dd>
-
-            <dt>備考</dt>
-            <dd>{{ note }}</dd>
-        </dl>
-        <button v-on:click="pointUp">ポイント加算</button>
+        <p>子コンポーネントで親の乱数を表示: {{ rand }}</p>
+        <button v-on:click="onNewRandButtonClick">親の乱数発生イベントを実行</button>
     </section>
 </template>
 
